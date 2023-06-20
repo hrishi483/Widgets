fa9b86e3fe648693936994e52b4a4b630b4348ec
hrishi483 <118972159+hrishi483@users.noreply.github.com>
2023-06-20 17:54:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
 #     version='0.0.3',
     author='Hrishikesh',
     author_email='hrishikeshkarande2020@gmail.com',
-    url='https://github.com/hrishi483/Widgets1.1.git',
+    url='https://github.com/hrishi483/Widgets.git',
     packages=find_packages(),
     install_requires=['panel','ipywidgets','openai','langchain']
 )
